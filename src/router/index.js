681e1af2b9130e68f9039f89aacf75720abb9af4
Ting-Jun Wang <levi900227@gmail.com>
2023-05-18 15:23:29 +0800

    fix: usseless page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import FaucetView from '../views/FaucetView.vue'
-import ManageView from '../views/ManageView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
